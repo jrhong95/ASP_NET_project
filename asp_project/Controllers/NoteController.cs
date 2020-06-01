b8078c97b5ecb4687d0cef86d8a56c2789a4fd4f
ahnsanghyeok <48887925+jooa7878@users.noreply.github.com>
2020-06-01 18:12:15 +0900

    Add files via upload

---
+++ asp_project/Controllers/NoteController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3.DataContext;
+using WebApplication3.Models;
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication3.Controllers
+{
+    public class NoteController : Controller
+    {
+        
+
+        /// <summary>
+        ///  게시판 리스트
+        /// </summary>
+        /// <returns></returns>
+
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            
+            using (var db = new NoteDBContext()) 
+            {
+                var list = db.Notes.ToList();
+                return View(list);
+            }
+
+            return View();
+        }
+        /// <summary>
+        /// 게시판 상세
+        /// </summary>
+        /// <param name="noteNo"></param>
+        /// <returns></returns>
+        public IActionResult Detail(int noteNo)
+        {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            using(var db = new NoteDBContext())
+            {
+                var note = db.Notes.FirstOrDefault(n => n.NoteNo.Equals(noteNo));
+                
+                return View(note);
+            }
+            return View();
+        }
+
+        /// <summary>
+        /// 게시물 추가
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Add()
+        {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Note model)
+        {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            model.UserNo = int.Parse(HttpContext.Session.GetInt32("USER_LOGIN_KEY").ToString());
+
+            if (ModelState.IsValid)
+            {
+                using (var db = new NoteDBContext())
+                {
+                    db.Notes.Add(model);
+                    if(db.SaveChanges() > 0)
+                    {
+                        return Redirect("Index");
+                    }
+                }
+                ModelState.AddModelError(string.Empty,"게시물을 저장할 수 없습니다.");
+            }
+
+            return View(model);
+        }
+
+
+        /// <summary>
+        ///  게시물 수정
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Edit()
+        {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            return View();
+        }
+        /// <summary>
+        /// 게시물 삭제
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Delete()
+        {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            return View();
+        }
+        
+    }
+}
