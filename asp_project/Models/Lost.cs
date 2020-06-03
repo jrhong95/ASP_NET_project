b009ccbe33d148207778c769931f78312a0e4b26
SehwaGod <jrhong95@gmail.com>
2020-06-04 00:22:01 +0900

    lost Update

---
+++ asp_project/Models/Lost.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebApplication3.Models
+{
+    public class Lost
+    {
+        public string location { get; set; }
+        public string goods { get; set; }
+        public string description { get; set; }
+        public string date { get; set; }
+    }
+}
